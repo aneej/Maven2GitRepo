7a2b57b80e5d3531da4c622df40b6041e940925a
aneej <aneej@hotmail.com>
2016-05-28 12:56:47 +0530

    this commit is to move the file to stages area

--- src/main/java/com/knowledgeranch/App.java
+++ src/main/java/com/knowledgeranch/App.java
@@ -4,10 +4,11 @@ package com.knowledgeranch;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+		// Adding this comment
         System.out.println( "Hello World!" );
     }
 }
